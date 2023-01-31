ee179e006dcb90f34feda54cfeed144260f9c6ca
Hazelboyaalici <hazelboyaalici@gmail.com>
2023-01-31 16:21:34 +0300

    ilk commit

---
+++ project3/Form1.cs
@@ -0,0 +1,26 @@
+namespace project3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ADINIZ_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text;
+
+        }
+    }
+}
